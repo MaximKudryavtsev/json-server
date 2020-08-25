bc2e449f3dd1a4e64c58c9873f0c455881922018
maksimkudryavtsev <maxim.kudryavtstev@omega-r.com>
2020-08-25 17:01:39 +0300

    add simple json server

--- server.js
+++ server.js
@@ -8,21 +8,26 @@ const FileSync = require('lowdb/adapters/FileSync')
 const adapter = new FileSync('db.json')
 const db = low(adapter)
 
-server.post("/users", (req, res) => {
-    const data = req.body;
-    db.get("users").push({
-        name: data.name,
-        username: data.username,
-        email: data.email,
-        password: data.password
-    }).write();
-    res.jsonp({
-        success: true
-    });
+server.use(middlewares)
 
+server.use(jsonServer.bodyParser)
+server.use("/users", (req, res, next) => {
+    if (req.method === 'POST') {
+        const data = req.body;
+        db.get("users").push({
+            name: data.name,
+            username: data.username,
+            email: data.email,
+            password: data.password
+        }).write();
+        res.jsonp({
+            success: true
+        });
+    }
+    // Continue to JSON Server router
+    next()
 })
 
-server.use(middlewares)
 server.use(router)
 server.listen(3002, () => {
     console.log('JSON Server is running')
