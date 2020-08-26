4ac213667e4ebb4e1d284f72ee659a7a31252d52
maksimkudryavtsev <maxim.kudryavtstev@omega-r.com>
2020-08-26 12:12:53 +0300

    add new routes

--- server.js
+++ server.js
@@ -11,6 +11,68 @@ const db = low(adapter)
 server.use(middlewares)
 
 server.use(jsonServer.bodyParser)
+
+server.get("/users", (req, res) => {
+    const data = db.get("users").value();
+    res.jsonp(data);
+})
+
+server.get("/posts", (req, res) => {
+    const data = db.get("posts").value();
+    res.jsonp(data);
+})
+
+server.get("/comments", (req, res) => {
+    const data = db.get("comments").value();
+    res.jsonp(data);
+})
+
+server.get("/albums", (req, res) => {
+    const data = db.get("albums").value();
+    res.jsonp(data);
+})
+
+server.get("/photos", (req, res) => {
+    const data = db.get("photos").value();
+    res.jsonp(data);
+})
+
+server.get("/todos", (req, res) => {
+    const data = db.get("todos").value();
+    res.jsonp(data);
+})
+
+
+server.get("/posts/:id/comments", (req, res) => {
+    const data = db.get("comments").find({postId: Number(req.params.id)}).value();
+    res.jsonp(data);
+})
+
+server.get("/users/:id", (req, res) => {
+    const data = db.get("users").find({id: Number(req.params.id)}).value();
+    res.jsonp(data);
+})
+
+server.get("/albums/:id/photos", (req, res) => {
+    const data = db.get("photos").find({albumId: Number(req.params.id)}).value();
+    res.jsonp(data);
+})
+
+server.get("/users/:id/albums", (req, res) => {
+    const data = db.get("albums").find({userId: Number(req.params.id)}).value();
+    res.jsonp(data);
+})
+
+server.get("/users/:id/todos", (req, res) => {
+    const data = db.get("todos").find({userId: Number(req.params.id)}).value();
+    res.jsonp(data);
+})
+
+server.get("/users/:id/posts", (req, res) => {
+    const data = db.get("posts").find({userId: Number(req.params.id)}).value();
+    res.jsonp(data);
+})
+
 server.use("/users", (req, res) => {
     if (req.method === 'POST') {
         const data = req.body;
